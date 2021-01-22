f245704519709f1fc9403a7aa6d7a63a25f47a4c
AmishRanpariya <amishranpariya@gmail.com>
2021-01-22 19:06:38 +0530

    adding PWA

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+
+//for PWA
+if('serviceWorker' in navigator){
+  navigator.serviceWorker.register('/sw.js')
+    .then(reg => console.log('service worker registered'))
+    .catch(err => console.log('service worker not registered', err));
+}
+
+
+
+
+//main program
+
 class Calc {
   constructor(previousOperandTextElement, currentOperandTextElement) {
     this.previousOperandTextElement = previousOperandTextElement;
