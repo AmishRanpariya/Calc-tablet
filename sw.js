3de6e12ad644479d80e3b705e2d14863f661ae0f
AmishRanpariya <amishranpariya@gmail.com>
2021-01-25 16:49:34 +0530

    refactoring

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 // install event
 self.addEventListener('install', evt => {
-  console.log('service worker installed');
+  // console.log('service worker installed');
 });
 
 // activate event
 self.addEventListener('activate', evt => {
-  console.log('service worker activated');
+  // console.log('service worker activated');
 });
 
 // fetch event
 self.addEventListener('fetch', evt => {
-  console.log('fetch event', evt);
+  // console.log('fetch event', evt);
 });
